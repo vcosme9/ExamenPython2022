1b7a4cb51117e6b1713b2c51df2e04c57c1a89d7
Vicent Cosme <vicentcosme6@gmail.com>
2022-03-07 11:35:02 +0100

    checkpoint 2

--- wordle.py
+++ wordle.py
@@ -7,7 +7,7 @@ def choose_secret(filename):
     Returns:
       secret: Palabra elegida aleatoriamente del fichero transformada a mayúsculas. Ej. "CREMA"
     """
-    with open(f'C:\Users\Usuario\Desktop\SEMESTRE 3B\PROYECTO\PYTHON\Examen\ExamenPython2022\{filename}') as f:
+    with open(r'C:\Users\Usuario\Desktop\SEMESTRE 3B\PROYECTO\PYTHON\Examen\ExamenPython2022\{filename}' , 'r') as f:
         wordssl = [line.strip() for line in f]
     words = wordssl
     secret = random.choice(words).upper()
@@ -35,7 +35,7 @@ def compare_words(word, secret):
     return same_position, same_letter
 
 
-def print_word():
+def print_word(word, same_letter_position, same_letter):
     """Dada una palabra, una lista same_position y otra lista same_letter, esta función creará un string donde aparezcan en mayúsculas las letras de la palabra que ocupen las posiciones de same_position, en minúsculas las letras de la palabra que ocupen las posiciones de same_letter y un guión (-) en el resto de posiciones
     Args:
       word: Una palabra. Ej. "CAMPO"
@@ -44,7 +44,17 @@ def print_word():
     Returns:
       transformed: La palabra aplicando las transformaciones. En el caso anterior: "Cam--"
     """
-    
+    transformed = []
+  
+    if len(same_letter_position) > 0:
+      for i in same_letter_position:
+        transformed.append(word[i])
+    if len(same_letter) > 0:
+      for j in same_letter:
+       transformed.append(word[j])
+
+    return transformed 
+
 def choose_secret_advanced():
     """Dado un nombre de fichero, esta función filtra solo las palabras de 5 letras que no tienen acentos (á,é,í,ó,ú). De estas palabras, la función devuelve una lista de 15 aleatorias no repetidas y una de estas 15, se selecciona aleatoriamente como palabra secret.
     Args:
@@ -63,6 +73,7 @@ def check_valid_word():
     """
 
 if __name__ == "__main__":
+    print(print_word('CAMPO', [0], [1,2])) 
     secret=choose_secret()
     print("Palabra a adivinar: "+secret)#Debug: esto es para que sepas la palabra que debes adivinar
     for repeticiones in range(0,6):
